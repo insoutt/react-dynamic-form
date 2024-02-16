c5db8accba3952d57510b88397a74a8fa2c651aa
insoutt <elvisfernando9@hotmail.com>
2024-02-15 20:05:31 -0500

    Refactor

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,7 +4,9 @@ import { FormProps } from '../utils/types';
 import { cn } from '../utils/utils';
 import { SimpleFormContext } from "../contexts/simple-form-context";
 
-const Form = <T extends FieldValues>({fields, validator, beforeSubmit, afterSubmit, className, classNames, isLoading, onSubmit, onClear, submitText, clearText, loadingText, hideClearButton, validateOnSubmit, children}: FormProps<T>) => {
+const Form = <T extends FieldValues>({fields, validator, beforeSubmit, afterSubmit, className, classNames, isLoading, onSubmit, onClear, loadingText, hideClearButton, validateOnSubmit, children, submitText = 'Submit', clearText = 'Clear'}: FormProps<T>) => {
+    loadingText = loadingText || submitText;
+
     const methods = useForm<T>();
 
     const formValues = methods.watch();
@@ -68,11 +70,11 @@ const Form = <T extends FieldValues>({fields, validator, beforeSubmit, afterSubm
                     ? children(methods.handleSubmit(formSubmit), clear)
                     : typeof children !=='undefined' ? children : <div className="form-action-buttons">
                             <button className={cn(classNames?.submitButton || 'btn btn-primary')} type="submit" disabled={isFormBusy}>
-                                {isFormBusy ? loadingText : submitText || 'Submit'}
+                                {isFormBusy ? loadingText : submitText}
                             </button>
                             {!hideClearButton && !isFormBusy && !isFormClear() && <>
                                 <button className={cn(classNames?.clearButton)} onClick={clear}>
-                                    {clearText || 'Clear'}
+                                    {clearText}
                                 </button>
                             </>}
                         </div>}
